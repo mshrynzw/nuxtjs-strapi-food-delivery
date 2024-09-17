21a3f12625ede5990bfd5b32f8ab9806c600e819
mshrynzw <iq87io25@gmail.com>
2024-09-17 15:41:15 +0900

    Add: Header

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white dark:bg-gray-950">
+    <Header/>
+    <div class="overflow-hidden">
+      <section class="relative">
+        <div class="relative py-24 lg:py-28">
+          <slot/>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "~/components/header/Header.vue"
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col space-y-8">
+    <form class="flex flex-row space-x-4" data-shade="900">
+      <button class="btn variant-solid sz-sm intent-primary">Generate</button>
+      <div class="field">
+        <input type='email' class="input variant-mixed sz-md" placeholder="Your email"/>
+      </div>
+    </form>
+
+    <div class="card variant-mixed max-w-xs" data-shade="900">
+      <span class="text-caption text-xs">Great UX</span>
+      <h3 class="mt-2 text-title text-sm font-medium">Realtime insights</h3>
+      <p class="text-sm mt-3">Provident fugit vero voluptate. dolores voluptates inventore nisi.</p>
+
+      <div class="mt-16">
+        <div class="text-caption text-xs">Loading 60%</div>
+        <div class="mt-3 progress sz-sm relative">
+          <div class="progress-indicator bg-gradient-to-r from-primary-600 to-accent-500 w-[60%]"></div>
+        </div>
+        <div class="w-[60%] blur-sm h-6 bg-gradient-to-b from-accent-500/10 to-transparent"></div>
+      </div>
+    </div>
+  </div>
+</template>
