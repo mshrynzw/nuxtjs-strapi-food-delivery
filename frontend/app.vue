e4dc714716bb2d2cce283bf9ed502c3e87d34cde
mshrynzw <iq87io25@gmail.com>
2024-09-17 11:48:14 +0900

    Init Frontend

---
+++ frontend/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold text-blue-600 mb-4">
+      Welcome to Nuxt with Tailwind CSS!
+    </h1>
+    <p class="text-gray-700">
+      This is a sample page using Tailwind CSS classes.
+    </p>
+  </div>
+</template>
